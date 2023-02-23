f0075578645f7a243c14698fd28a595f75c2a1a5
lys7375 <yushuolu7375@gmail.com>
2023-02-22 17:42:19 -0800

    create a player and add animation

--- MiniMatchMage/GameScene.swift
+++ MiniMatchMage/GameScene.swift
@@ -13,6 +13,66 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    var player : SKSpriteNode!
+    var enemy1 : SKSpriteNode!
+    
+    private var playerAtlas: SKTextureAtlas {
+        return SKTextureAtlas(named: "Player1Animation")
+    }
+    
+//    private var enemy1Atlas: SKTextureAtlas {
+//        return SKTextureAtlas
+//    }
+
+    private var playerTexture: SKTexture {
+        return playerAtlas.textureNamed("Player1Animation")
+    }
+    
+    private var playerAttackTextures: [SKTexture] {
+        return [
+            playerAtlas.textureNamed("attack_1"),
+            playerAtlas.textureNamed("attack_2"),
+            playerAtlas.textureNamed("attack_3"),
+            playerAtlas.textureNamed("attack_4"),
+            playerAtlas.textureNamed("attack_5")
+        ]
+    }
+    
+    private var playerIdleTextures: [SKTexture] {
+        return [
+            playerAtlas.textureNamed("idle_1"),
+            playerAtlas.textureNamed("idle_2"),
+            playerAtlas.textureNamed("idle_3"),
+            playerAtlas.textureNamed("idle_4")
+        ]
+    }
+    
+    // set up the image
+    private func setupPlayerIdle() {
+        // set the texture size
+        player = SKSpriteNode(texture: playerTexture, size: CGSize(width: 350, height: 350))
+      // set the texture position
+        player.position = CGPoint(x: -250, y: 100)
+        
+        addChild(player)
+    }
+    
+    func startAttackAnimation() {
+        // set the animation duration
+        let attackAnimation = SKAction.animate(with: playerAttackTextures, timePerFrame: 0.1)
+
+        // run the animation
+        player.run(SKAction.repeatForever(attackAnimation), withKey: "playerAttackTextures")
+    }
+
+    func startIdleAnimation() {
+        // set the animation duration
+        let idleAnimation = SKAction.animate(with: playerIdleTextures, timePerFrame: 0.1)
+
+        // run the animation
+        player.run(SKAction.repeatForever(idleAnimation), withKey: "playerIdleTextures")
+    }
+    
     override func didMove(to view: SKView) {
         
         // Get label node from scene and store it for use later
@@ -34,6 +94,10 @@ class GameScene: SKScene {
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
         }
+        
+        self.setupPlayerIdle()
+        self.startAttackAnimation()
+//        self.startIdleAnimation()
     }
     
     
@@ -84,5 +148,6 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
     }
 }
