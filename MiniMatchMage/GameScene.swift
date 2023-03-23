939840ff75154d434f3e1c27dbe72eab75a79545
hushendao <hushendao@gmail.com>
2023-03-22 19:23:49 -0700

    Revert "Merge remote-tracking branch 'origin/develop' into Shendao_dev"
    This reverts commit 34030b604456f0d92a9117bf428a5012f34fc8b1, reversing
    changes made to e56021f9c7654cca2032f522753941bbf659799e.

--- MiniMatchMage/GameScene.swift
+++ MiniMatchMage/GameScene.swift
@@ -12,129 +12,6 @@ class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
-    private var background = SKSpriteNode(imageNamed: "bg1.jpeg")
-    
-    var player : SKSpriteNode!
-    var enemy1 : SKSpriteNode!
-    var enemy2 : SKSpriteNode!
-    var enemy3 : SKSpriteNode!
-    
-    private var playerAtlas: SKTextureAtlas {
-        return SKTextureAtlas(named: "Player1Animation")
-    }
-    
-    private var enemy1Atlas: SKTextureAtlas {
-        return SKTextureAtlas(named: "Enemy1Animation")
-    }
-
-    private var playerTexture: SKTexture {
-        return playerAtlas.textureNamed("Player1Animation")
-    }
-    
-    private var enemy1Texture: SKTexture {
-        return enemy1Atlas.textureNamed("Enemy1Animation")
-    }
-    
-    private var playerAttackTextures: [SKTexture] {
-        return [
-            playerAtlas.textureNamed("attack_1"),
-            playerAtlas.textureNamed("attack_2"),
-            playerAtlas.textureNamed("attack_3"),
-            playerAtlas.textureNamed("attack_4"),
-            playerAtlas.textureNamed("attack_5")
-        ]
-    }
-    
-    private var playerIdleTextures: [SKTexture] {
-        return [
-            playerAtlas.textureNamed("idle_1"),
-            playerAtlas.textureNamed("idle_2"),
-            playerAtlas.textureNamed("idle_3"),
-            playerAtlas.textureNamed("idle_4")
-        ]
-    }
-    
-    private var enemy1AttackTextures: [SKTexture] {
-        return [
-            enemy1Atlas.textureNamed("enemy_1_attack_0"),
-            enemy1Atlas.textureNamed("enemy_1_attack_1"),
-            enemy1Atlas.textureNamed("enemy_1_attack_2"),
-            enemy1Atlas.textureNamed("enemy_1_attack_3"),
-            enemy1Atlas.textureNamed("enemy_1_attack_4"),
-            enemy1Atlas.textureNamed("enemy_1_attack_5"),
-            enemy1Atlas.textureNamed("enemy_1_attack_6"),
-            enemy1Atlas.textureNamed("enemy_1_attack_7"),
-            enemy1Atlas.textureNamed("enemy_1_attack_8"),
-            enemy1Atlas.textureNamed("enemy_1_attack_9"),
-            enemy1Atlas.textureNamed("enemy_1_attack_10")
-        ]
-    }
-    
-    private var enemy1IdleTextures: [SKTexture] {
-        return [
-            enemy1Atlas.textureNamed("enemy_1_idle_0"),
-            enemy1Atlas.textureNamed("enemy_1_idle_1"),
-            enemy1Atlas.textureNamed("enemy_1_idle_2"),
-            enemy1Atlas.textureNamed("enemy_1_idle_3"),
-            enemy1Atlas.textureNamed("enemy_1_idle_4"),
-            enemy1Atlas.textureNamed("enemy_1_idle_5"),
-            enemy1Atlas.textureNamed("enemy_1_idle_6"),
-            enemy1Atlas.textureNamed("enemy_1_idle_7"),
-            enemy1Atlas.textureNamed("enemy_1_idle_8")
-        ]
-    }
-    
-    // set up the image
-    private func setupPlayer() {
-        // set the texture size
-        player = SKSpriteNode(texture: playerTexture, size: CGSize(width: 350, height: 350))
-      // set the texture position
-        player.position = CGPoint(x: -250, y: 100)
-        
-        addChild(player)
-    }
-    
-    private func setupEnemy1() {
-        enemy1 = SKSpriteNode(texture: enemy1Texture, size: CGSize(width: 250, height: 250))
-        enemy1.position = CGPoint(x: 200, y : 100)
-        enemy2 = SKSpriteNode(texture: enemy1Texture, size: CGSize(width: 250, height: 250))
-        enemy2.position = CGPoint(x: 120, y : 100)
-        enemy3 = SKSpriteNode(texture: enemy1Texture, size: CGSize(width: 250, height: 250))
-        enemy3.position = CGPoint(x: 280, y : 100)
-        addChild(enemy1)
-        addChild(enemy2)
-        addChild(enemy3)
-    }
-    
-    func startAttackAnimation() {
-        // set the animation duration
-        let attackAnimation = SKAction.animate(with: playerAttackTextures, timePerFrame: 0.1)
-
-        // run the animation
-        player.run(SKAction.repeatForever(attackAnimation), withKey: "playerAttackTextures")
-    }
-    
-    func startEnemy1AttackAnimation() {
-        let attackAnimation = SKAction.animate(with: enemy1AttackTextures, timePerFrame: 0.1)
-        
-        enemy1.run(SKAction.repeatForever(attackAnimation), withKey: "enemy1AttackTextures")
-    }
-
-    func startIdleAnimation() {
-        // set the animation duration
-        let idleAnimation = SKAction.animate(with: playerIdleTextures, timePerFrame: 0.1)
-
-        // run the animation
-        player.run(SKAction.repeatForever(idleAnimation), withKey: "playerIdleTextures")
-    }
-    
-    func startEnemy1IdleAnimation() {
-        let idleAnimation = SKAction.animate(with: enemy1IdleTextures, timePerFrame: 0.1)
-        
-        enemy1.run(SKAction.repeatForever(idleAnimation), withKey: "enemy1IdleTextures")
-        enemy2.run(SKAction.repeatForever(idleAnimation), withKey: "enemy1IdleTextures")
-        enemy3.run(SKAction.repeatForever(idleAnimation), withKey: "enemy1IdleTextures")
-    }
     
     override func didMove(to view: SKView) {
         
@@ -157,20 +34,6 @@ class GameScene: SKScene {
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
         }
-        
-        background.position = CGPoint(x: 0, y: 260)
-        background.size.width = self.size.width
-        background.size.height = self.size.height
-        background.anchorPoint = CGPoint(x: 0.5,y: 0.5)
-
-        self.addChild(background)
-        
-        self.setupPlayer()
-        self.startIdleAnimation()
-//        self.startAttackAnimation()
-        self.setupEnemy1()
-        self.startEnemy1IdleAnimation()
-//        self.startEnemy1AttackAnimation()
     }
     
     
@@ -221,6 +84,5 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-        
     }
 }
--- MiniMatchMage/GameViewController.swift
+++ MiniMatchMage/GameViewController.swift
@@ -31,8 +31,7 @@ class GameViewController: UIViewController {
                 view.presentScene(scene)
             }
             
-            
-            view.ignoresSiblingOrder = false
+            view.ignoresSiblingOrder = true
             
             view.showsFPS = true
             view.showsNodeCount = true
@@ -114,72 +113,25 @@ class GameViewController: UIViewController {
         }
     }
     
-//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        guard let touch = touches.first else { return }
-//
-//    }
-    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
-        let pos = touch.location(in: self.boardView)
-        let row = firstTileRow
-        let column = firstTileColumn
-        let closedRow = 1 - row
-        if boardTiles[row][column].imageView.frame.contains(pos) {
-            singleMatch(row, column)
-        } else if boardTiles[closedRow][column].imageView.frame.contains(pos) {
-            doubleMatch(row, closedRow, column, column)
-        } else if column-1>=0 && boardTiles[row][column-1].imageView.frame.contains(pos) {
-            doubleMatch(row, row, column-1, column)
-        } else if column+1<=numberOfColumns-1 && boardTiles[row][column+1].imageView.frame.contains(pos) {
-            doubleMatch(row, row, column, column+1)
-        } else if column-1>=0 && boardTiles[closedRow][column-1].imageView.frame.contains(pos) {
-            squareMatch(column-1, column)
-        } else if column+1<=numberOfColumns-1 && boardTiles[closedRow][column+1].imageView.frame.contains(pos) {
-            squareMatch(column, column+1)
+        for (row, boardTilesRow) in boardTiles.enumerated() {
+            for (column, currentTile) in boardTilesRow.enumerated() {
+                if currentTile.imageView == touch.view {
+//                    print(firstTileRow)
+//                    print(firstTileColumn)
+//                    print(row)
+//                    print(column)
+                    if (row == firstTileRow && column == firstTileColumn) {
+                        singleMatch(row, column)
+                    }
+                    
+                }
+            }
         }
     }
     
-    // Single match tile
     func singleMatch(_ row: Int, _ column: Int) {
-        removeTile(row, column)
-    }
-    
-    // Double match tiles in line
-    func doubleMatch(_ row1: Int, _ row2: Int, _ column1: Int, _ column2: Int) {
-        let tiles = [boardTiles[row1][column1], boardTiles[row2][column2]]
-        if checkMatch(tiles) {
-            if row1 == row2 {
-                removeTwoTiles(row1, column1, column2)
-            } else {
-                removeTile(row1, column1)
-                removeTile(row2, column2)
-            }
-        }
-    }
-    
-    // Square match four tiles in square
-    func squareMatch(_ column1: Int, _ column2: Int) {
-        let tiles = [boardTiles[0][column1], boardTiles[0][column2], boardTiles[1][column1], boardTiles[1][column2]]
-        if checkMatch(tiles) {
-            removeTwoTiles(0, column1, column2)
-            removeTwoTiles(1, column1, column2)
-        }
-    }
-    
-    func checkMatch(_ tiles: [BoardTile]) -> Bool {
-        let tileNo = tiles.first?.tileNo
-        var result: Bool = true
-        for tile in tiles {
-            if tile.tileNo != tileNo {
-                result = false
-                break
-            }
-        }
-        return result
-    }
-    
-    func removeTile(_ row: Int, _ column: Int) {
         var currentTile = boardTiles[row][column]
         UIView.animate(withDuration: clearTileAnimateDuration) {
             currentTile.imageView.transform = CGAffineTransform.identity.scaledBy(x: 0.5, y: 0.5)
@@ -197,34 +149,7 @@ class GameViewController: UIViewController {
             currentTile.imageView.transform = CGAffineTransform.identity.scaledBy(x: 1, y: 1)
             currentTile.imageView.frame.origin.x += CGFloat(numberOfColumns - column) * self.tileSize
             self.boardTiles[row][numberOfColumns - 1] = currentTile
-        }
-    }
-    
-    func removeTwoTiles(_ row: Int, _ column1: Int, _ column2: Int) {
-        var currentTile1 = boardTiles[row][column1]
-        var currentTile2 = boardTiles[row][column2]
-        UIView.animate(withDuration: clearTileAnimateDuration) {
-            currentTile1.imageView.transform = CGAffineTransform.identity.scaledBy(x: 0.5, y: 0.5)
-            currentTile2.imageView.transform = CGAffineTransform.identity.scaledBy(x: 0.5, y: 0.5)
-        }
-        DispatchQueue.main.asyncAfter(deadline: .now() + clearTileAnimateDuration) {
-            UIView.animate(withDuration: moveTileAnimateDuration) {
-                for columnToMove in ((column2 + 1)..<numberOfColumns) {
-                    let tile = self.boardTiles[row][columnToMove]
-                    tile.imageView.frame.origin.x -= self.tileSize * 2
-                    self.boardTiles[row][columnToMove - 2] = tile
-                }
-                currentTile1.imageView.frame.origin.x -= self.tileSize * 2
-                currentTile1.tileNo = Int.random(in: tileNoRange)
-                currentTile2.imageView.frame.origin.x -= self.tileSize * 2
-                currentTile2.tileNo = Int.random(in: tileNoRange)
-            }
-            currentTile1.imageView.transform = CGAffineTransform.identity.scaledBy(x: 1, y: 1)
-            currentTile2.imageView.transform = CGAffineTransform.identity.scaledBy(x: 1, y: 1)
-            currentTile1.imageView.frame.origin.x += CGFloat(numberOfColumns - column1) * self.tileSize
-            currentTile2.imageView.frame.origin.x += CGFloat(numberOfColumns - column2 + 1) * self.tileSize
-            self.boardTiles[row][numberOfColumns - 2] = currentTile1
-            self.boardTiles[row][numberOfColumns - 1] = currentTile2
+            
         }
     }
 }
