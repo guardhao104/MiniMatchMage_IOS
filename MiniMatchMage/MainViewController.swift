c7931aa7f0b28a1e25902a2c2c1f0c50ee42cce0
lys7375 <yushuolu7375@gmail.com>
2023-03-29 20:29:57 -0700

    Modify MainPage UI

--- MiniMatchMage/MainViewController.swift
+++ MiniMatchMage/MainViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import SpriteKit
 
 class MainViewController: UIViewController {
-    
+    let image = UIImage(named: "myImage")
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,16 +17,20 @@ class MainViewController: UIViewController {
         myView.backgroundColor = UIColor.white
         self.view.addSubview(myView)
                 
+        let image = UIImage(named: "button")
+        
         let backgroundImageView = UIImageView(frame: myView.bounds)
-        backgroundImageView.image = UIImage(named: "bg1.jpg")
+        backgroundImageView.image = UIImage(named: "bg4.jpg")
         myView.addSubview(backgroundImageView)
                 
         let button = UIButton(type: .system)
-        button.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
-        button.setTitle("My Button", for: .normal)
+        button.frame = CGRect(x: (self.view.bounds.width - 250)/2, y: 250, width: 250, height:100)
+        button.setTitle("START", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 30)
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-        button.backgroundColor = .white
+//        button.backgroundColor = .white
         button.setTitleColor(.black, for: .normal)
+        button.setBackgroundImage(image, for: .normal)
         myView.addSubview(button)
 
     }
